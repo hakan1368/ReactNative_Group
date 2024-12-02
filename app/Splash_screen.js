1d20ca9c542ca68744229df20e76ae206af4303b
Debian-code <raphaelmarques92200@gmail.com>
2024-12-02 08:30:49 +0100

    Splash

--- app/Splash_screen.js
+++ app/Splash_screen.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image} from 'react-native';
 
 const SplashScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to Linguini!</Text>
       <Text style={styles.subtitle}>Please wait while we load your app...</Text>
+      <Image
+        source={require('../assets/images/logo.webp')}
+        style={styles.image}
+      />
     </View>
   );
 };
@@ -20,13 +24,19 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 32,
     fontWeight: 'bold',
-    color: '#3498db',  // Same blue color as in AuthScreen
+    color: 'black',  // Same blue color as in AuthScreen
     marginBottom: 10,  // Spacing below the title
   },
   subtitle: {
     fontSize: 18,
     color: '#777',  // Light gray color for the subtitle
   },
+  image : {
+    width: '50%',
+    height: 200,
+    borderRadius: 8,
+    margin: 50
+  }
 });
 
 export default SplashScreen;
